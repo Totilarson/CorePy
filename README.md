1a711f377a6b1cd4e5b384dd18d0d330b96c9b0f
Esben Pedersen <esp.pedersen@gmail.com>
2020-10-07 21:45:59 +0200

    Cleanup and additional documentation
    - added additional line based documentation
    - minor maintenance and revision

--- README.md
+++ README.md
@@ -1,24 +1,25 @@
 # CorePy
-Python tools designed to integrate and visualize geological core data.
-This repository hosts a series of scripts that are used to evaluate 
+CorePy is a series of tools written in Python that are designed to integrate and visualize geological core data.
+This public repository hosts a series of scripts that are used to evaluate
 and cluster geological core-based X-ray fluorescence (XRF) measurements.
-These Python tools were developed by Toti Larson and Esben Pederson at the 
-University of Texas at Austin (UT Austin) Bureau opf Economic Geology (BEG) 
+These Python tools were developed by Toti Larson and Esben Pedersen at the
+University of Texas at Austin (UT Austin) Bureau of Economic Geology (BEG)
 Mudrock Systems Research laboratory (MSRL).
 
 
-Scripts
-1) settings: this houses the variables used in several scripts and the 
-directory structure. 
-2) PCAchemofacies: this script a) sets instrument detection limits into the
+Scripts included in this public release of CorePy:
+1) settings.py: this script houses the variables used in several scripts and defines the
+directory structure and input/output file handling for usage in subsequent scripts.
+2) PCAchemofacies.py: this script a) sets instrument detection limits into the
 XRF dataset, b) identifies outliers based on mean + 4 standard deviations, c)
-conducts a principal component analysis (PCA), d) runs a k-means cluster analysis on
-resutling PCA for unsupervised chamoefacies classification, e) plots PCA and cluster
-results on a biplot, and f) outputs a new dataset with cluster identifications on one 
-column.
+conducts a principal component analysis (PCA), d) runs a k-means cluster analysis on resulting PCA for unsupervised chemofacies classification, e) plots PCA and cluster results on a biplot, and f) outputs a new dataset with cluster identifications in an appended column.
 
 
-Initial Folder structure:
-1) Set up a Folder called "CorePy" and map coredata_dir (settings) to it. 
+Initial setup instructions & directory handling:
+1) Create a local folder called "CorePy" and map coredata_dir (edit the respective line(s) in settings.py) to it.
 For example:'C:/Users/' + user + '/Box/CorePy/Coredata/CoreXRF'
-2) set up /Coredata/CoreXRF/ and place the file "Core-01" in that directory
+2) Set up /Coredata/CoreXRF/ and place the file "Core-01" in that directory (Example dataset to test PCAchemofacies.py)
+3) Run Settings.py
+4) Run PCAchemofacies.py
+
+NOTE That it may be necessary to install python packages in addition to those provided by your default python distribution in order for this version of CorePy to run and operate as intended.
--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 # Author: Toti Larson & Esben Pedersen
 # Date:   09/16/2020
 # Description:  settings.py is run first and is where variables used in subsequent scripts are defined and listed
-# Folder structure (i.e., 'C:/Users/') may need to be changed depending or your system 
+# Folder structure (i.e., 'C:/Users/') may need to be changed depending or your system
 # Version: 1.0
 #   Changelog:
 #==========================================
@@ -12,48 +12,47 @@
 import os
 
 # Define settings.py variables
+user = "larsont"        # Used to identify file structure
+corename = 'Core-01'    # Core name (set to use the example dataset provided)
+coretube_name = 'C1'    # Two letters identifying each coretube image
+photo_type = 'Vis'      # Options are Vis or UV
+coretube_length = 2     # Length (in feet) of coretube images for depth referencing
+noOfcolumns = 5         # Number of columns within each core box
 
-user = "larsont"        # used to identify file structure
-corename = 'Core-01' #core name
-coretube_name = 'C1'    #two letters identifying each coretube image
-photo_type = 'Vis'      # VIS or UV are the options
-coretube_length = 2     #length in feet of coretube images
-noOfcolumns = 5
+# Input list of Formations as strings, or simply use ['all'] to include all formations.
+Formation = ['Eagle Ford'] # List of strings, for example: ['Pine Island', 'Cow Creek']
 
-# Input list of Formations as strings, or simply use ['all'] to include all formations. 
-Formation = ['Eagle Ford'] # List of strings
+Formation_names = '-'.join(Formation) # For output file naming purposes, will join Formations in var Formation
 
-Formation_names = '-'.join(Formation) # For output name purposes
+XRF_resolution = 2/12 # Defines vertical resolution of XRF scans (2 inches)
 
-XRF_resolution=2/12
-
-elements =  ['Na', 'Mg', 'Al', 'Si', 'P', 'S', 'K', 'Ca', 'Ti','Mn', 'Fe', 'Ba', 'V', 'Cr', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'As', 'Pb', 
-             'Th', 'Rb', 'U', 'Sr', 'Y', 'Zr', 'Nb', 'Mo'] #elements used in all PCA and neural network models
+elements =  ['Na', 'Mg', 'Al', 'Si', 'P', 'S', 'K', 'Ca', 'Ti','Mn', 'Fe', 'Ba', 'V', 'Cr', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'As', 'Pb',
+             'Th', 'Rb', 'U', 'Sr', 'Y', 'Zr', 'Nb', 'Mo'] # Elements used in all PCA and neural network models
 
 #'Depth_calculated' is calculated from core box top depth + sticker number
 #XRF_adjusted_depth are adjusted to tie to wire line logs
-Depth_model='Depth_calculated' #'Depth_calculated' and 'XRF_adjusted_depth' are options in the data file. 
+Depth_model = 'Depth_calculated' # 'Depth_calculated' and 'XRF_adjusted_depth' are options in the data file.
 
 
 
-Ternary_elements=['Si', 'Al', 'Ca'] # elements plotted in ternary diagram
+Ternary_elements = ['Si', 'Al', 'Ca'] # elements used to be plotted in ternary diagram
 
-outlier_multiplier = 4      # used in the statistics to identify outliers
-clusters = 5                # number of K-means clusters used to cluster PCA
-Neural_model_chemofacies=6  # Number of chemofacies in Training dataset
+outlier_multiplier = 4          # Scalar chosen as cutoff to define outliers
+clusters = 5                    # Number of K-means clusters used to cluster PCA (Adjust to chose number of clusters defined)
+Neural_model_chemofacies = 6    # Number of chemofacies in Training dataset (var clusters+1, to account for outlier classification)
 
-#how many Principal components are used to build the chemofacies.
-Principal_components = 2    # number of principal components applied to clustering
-PCA1=0  # principal component plotted on x-axis
-PCA2=1 # principal component plotted on y-axis
+# Define how many principal components are used to build the chemofacies.
+Principal_components = 2    # Number of principal components applied to clustering
+PCA1 = 0  # Principal component plotted on x-axis
+PCA2 = 1 # Principal component plotted on y-axis
 
-
+# Directory and input/output handling. Edit to your localization.
 coredata_dir = os.path.join('C:/Users/' + user + '/Box/CorePy/Coredata/CoreXRF')
 output_dir = os.path.join('C:/Users/' + user +   '/Box/CorePy/Coreoutput' + '/' + user + '/' + corename + ' Output' + '/' + Formation_names)
-coretube_dir = os.path.join('C:/Users/' + user + '/Box/CorePy/Coretubes') 
-corephotos_dir = os.path.join('C:/Users/' + user + '/Box/CorePy/Coredata/Corephotos') 
+coretube_dir = os.path.join('C:/Users/' + user + '/Box/CorePy/Coretubes')
+corephotos_dir = os.path.join('C:/Users/' + user + '/Box/CorePy/Coredata/Corephotos')
 crossection_dir = os.path.join('C:/Users/' + user + '/Box/CorePy/Coredata/Crossection/' + Formation_names)
-coredata_attributes = os.path.join('C:/Users/' + user + '/Box/CorePy/Coredata/Coredata/Coreattributes') 
+coredata_attributes = os.path.join('C:/Users/' + user + '/Box/CorePy/Coredata/Coredata/Coreattributes')
 neural_model = os.path.join('C:/Users/' + user + '/Box/CorePy/Coreoutput' + '/' + user + '/' + 'Neuralmodel')
 LOD_T5 = os.path.join(coredata_dir,'T5iLOD.csv')
 
@@ -62,6 +61,6 @@ if not os.path.exists(output_dir):
 if not os.path.exists(crossection_dir):
     os.makedirs(crossection_dir)
 
-
+# Defines suffix files for input data and image files
 suffix =  '.csv'
 imagesuffix = '.png'
