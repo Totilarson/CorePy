05c10cb05c8411bcd979821fdc74ee66032662d6
Toti Larson <65969309+Totilarson@users.noreply.github.com>
2021-06-06 16:03:40 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ with open ("README.md", "r") as fh:
     
 setup(   # lines of configuration
       name='corepytools',             #name is what you PIP install and what you will upload it as. Not the same as py_modules
-      version='0.0.1',
+      version='0.0.3',
       description='CorePy: XRF clustering tools to interpret and visualize geological core data',
       long_description=long_description,
       long_description_content_type="text/markdown",
-      py_modules=["corepy"],    # list of actual python code modules
+      py_modules=["corepytools"],    # list of actual python code modules
+      #py_modules=["corepy"],    # list of actual python code modules
       package_dir={'':'src'},       # says that our code is under a src directory
       url='https://github.com/Totilarson/CorePy',
       author='Toti Larson',
