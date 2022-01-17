c316f9b0157e5b86bfdc31fe5f369c1018a43578
Toti Larson <toti.larson@beg.utexas.edu>
2022-01-17 10:33:09 -0600

    completed directories

--- CorePycodes/NN_model_wireline_apply.py
+++ CorePycodes/NN_model_wireline_apply.py
@@ -6,31 +6,37 @@ import pickle
 #import seaborn as sns
 import json
 import corepytools as corepy
+import xgboost as xgb
 
+# This script applies XGBoost and NN supervised clustering algorithms on integrated XRF-Wireline log files
+# Input: NN_model_wireline_Formation and XGB_model_wireline_Formation in folder /CoreData/CoreNeuralModel/ 
+# Input: data file _WirelineLog.csv in selected core output folder 
+# Output: '_WirelineLog_NN.csv' file in selected core output folder 
+
+# Root_path, Run_settings, and Corebeta and the two .json core settings files with all input parameters
 Root_path = os.path.dirname(os.getcwd())
 Run_settings=json.load(open(os.path.join(Root_path + '/CorePycodes/' + 'Run_settings' + '.json')))
 Corebeta=json.load(open(os.path.join(Root_path + '/CoreData/CoreBeta/'   +  Run_settings['CoreOfStudy']  +'.json')))
 
+# Formation_names is an expansion idea to select sub-Formations
+# Creates a str variable to select Formation-specific rows from csv input file. 
+# For now Formation_names is Run_settings["Formation"]
 Formation_names = '-'.join(Run_settings["Formation"]+Run_settings["Formation_2"]) # Would like to have Formation_names defined in Corebeta
 
-
+# had to add separate dirName directory to link to _Wirelinelog.csv file
 dirName=corepy.RootDir(Run_settings["CoreOfStudy"], Formation_names) 
+coredata = (os.path.join(dirName + '/' +  Run_settings['CoreOfStudy'] + '_' + Formation_names + '_WirelineLog.csv'))
+coredata=pd.read_csv(coredata)
 
-
+# direct to machine learning models
 NN_file=os.path.join(Root_path + '/CoreData/CoreNeuralModel/' + 'NN_model_wireline_' + Formation_names)
-
-
+XGB_file=os.path.join(Root_path + '/CoreData/CoreNeuralModel/' + 'XGB_model_wireline_' + Formation_names)
 
 infile = open(NN_file,'rb')
 NN_model= pickle.load(infile)
 infile.close()
 
-#coredata = corepy.OutputXRF(Run_settings["CoreOfStudy"],Formation_names)
-
-coredata = (os.path.join(dirName + '/' +  Run_settings['CoreOfStudy'] + '_' + Formation_names + '_WirelineLog.csv'))
-coredata=pd.read_csv(coredata)
-
-
+# Wireline logs are user selected. data stored in Corebeta["WirelineLogs_NeuralModel"]
 Model_logs= Corebeta["WirelineLogs_NeuralModel"]
 
 X = coredata[Model_logs].values #makes an array of elements in coredata df
@@ -38,13 +44,7 @@ X = coredata[Model_logs].values #makes an array of elements in coredata df
 # scale the data
 scaler = StandardScaler()
 scaler.fit(X)
-
 X_total = scaler.transform(X)
-
-
-#NeuralModel_TrainingDataSet = CoreWirelinedata
-
-
 
 chemo_predict=NN_model.predict(X_total) #scaled original dataset. 
 chemo_predict=chemo_predict.reshape(-1, 1)
@@ -56,19 +56,31 @@ NeuralModel_TrainingDataSet = os.path.join(Root_path + '/CoreData/CoreNeuralMode
 NeuralModel_TrainingDataSet = pd.read_csv(NeuralModel_TrainingDataSet).sort_values(by=[Run_settings["Depth_model"]], ascending=False)
 Chemofacies_count=np.sort(NeuralModel_TrainingDataSet['Chemofacies_NN'].unique())
 
-
-#Chemofacies_count=np.unique(chemo_predict) #problem when not all chemofacies are identified. Chemofacies count (Prediction matrix headings) doesn equal
-
 Prediction_matrix_headings=['Electrofacies_NN']
 for i in range(len(Chemofacies_count)):
     Prediction_matrix_headings.append('Prob'+str(Chemofacies_count[i]))
 
-#data=pd.DataFrame(np.concatenate((chemo_predict,chemo_prob),axis=1),columns = Prediction_matrix_headings)
 data=pd.DataFrame(np.concatenate((chemo_predict,chemo_prob),axis=1),columns = Prediction_matrix_headings)
 
-#data=pd.DataFrame(np.concatenate((chemo_predict,chemo_prob),axis=1),columns = ['Chemofacies_NN' , 'Prob1', 'Prob2' , 'Prob3', 'Prob4', 'Prob5','Prob6'])
+
+# Decision tree model
+infile = open(XGB_file,'rb')
+XGB_model= pickle.load(infile)
+infile.close()
+
+X = coredata[Model_logs]
+
+
+X_XGB = xgb.DMatrix(X)
+XGB_predict=XGB_model.predict(X_XGB) 
+y_pred_XGB = np.asarray([np.argmax(line) for line in XGB_predict]) # an array of the predictions for each sample (largest value)
+y_pred_XGB = pd.DataFrame(y_pred_XGB)
+y_pred_XGB.columns = ['Electrofacies_XGB']
+
 
 Z=pd.concat([coredata, data], ignore_index=False)
 Z = coredata.merge(data, left_index=True, right_index=True)
+Z = Z.merge(y_pred_XGB, left_index=True, right_index=True)
 
+#Output to .csv file in coredata output folder
 Z.to_csv (os.path.join(dirName + '/' +  Run_settings["CoreOfStudy"] + '_' + Formation_names + '_WirelineLog_NN.csv'))
--- CorePycodes/NN_wirelinemodel_build.py
+++ CorePycodes/NN_wirelinemodel_build.py
@@ -1,7 +1,3 @@
-# This script builds a chemofacies training dataset for the Formation listed in settings "Formation_names"
-# It searches for a Training dataset .csv file in the folder /CoreData/CoreNeuralModel/ and uses the chemofacies listed in column 'Chemofacies_NN'
-# Output is a file in folder /CoreData/CoreNeuralModel/ listed for the formation
-
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -15,36 +11,49 @@ import matplotlib.pyplot as plt
 import os
 import json
 
+import xgboost as xgb
+from xgboost import plot_importance
+
+# This script runs XGBoost and NN supervised clustering algorithms on integrated XRF-Wireline log files
+# Input: Labeled Training dataset _WirelineLog_TrainingDataset.csv file in the folder /CoreData/CoreNeuralModel/
+# Input: Wireline logs applied have to be specified CoreBeta["WirelineLogs_NeuralModel"] 
+# Output: NN_model_wireline_Formation and XGB_model_wireline_Formation in folder /CoreData/CoreNeuralModel/ 
+
+# Root_path, Run_settings, and Corebeta and the two .json core settings files with all input parameters
 Root_path = os.path.dirname(os.getcwd())
 Run_settings=json.load(open(os.path.join(Root_path + '/CorePycodes/' + 'Run_settings' + '.json')))
 Corebeta=json.load(open(os.path.join(Root_path + '/CoreData/CoreBeta/'   +  Run_settings['CoreOfStudy']  +'.json')))
 
+# Formation_names is an expansion idea to select sub-Formations
+# Creates a str variable to select Formation-specific rows from csv input file. 
+# For now Formation_names is Run_settings["Formation"]
 Formation_names = '-'.join(Run_settings["Formation"]+Run_settings["Formation_2"]) # Would like to have Formation_names defined in Corebeta
 
-
+# Import labelled training dataset. 
 NeuralModel_TrainingDataSet = os.path.join( Root_path + '/CoreData/CoreNeuralModel/'  + Formation_names   +   '_WirelineLog_TrainingDataset.csv')
 NeuralModel_TrainingDataSet = pd.read_csv(NeuralModel_TrainingDataSet).sort_values(by=[Run_settings["Depth_model"]], ascending=False)
 
 
-#NeuralModel_TrainingDataSet = CoreWirelinedata
+# User-selected logs for Wireline model are stored in the corbeta file. 
 Model_logs= Corebeta["WirelineLogs_NeuralModel"]
-# Making training dataset for Neural model
-y=NeuralModel_TrainingDataSet['Chemofacies_NN']
+
+
+## Neural Network Clustering model
+
+y=NeuralModel_TrainingDataSet['Chemofacies_NN'] # Making training dataset for Neural model
 X = NeuralModel_TrainingDataSet[Model_logs].values #converts X from a df to an array
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20,random_state=0)
 
-
-# scale the data
-scaler = StandardScaler()
+scaler = StandardScaler() # scale the data
 scaler.fit(X_train) # check this. why is it here and does it need to be used by all datasets
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 X_total = scaler.transform(X)
 
 # apply the neural network to the scaled data
-# solver{‘lbfgs’, ‘sgd’, ‘adam’}  sgd: stochastic gradient descent. adam: stochastic gradient-based optimizer. lbfgs: an optimizer in the family of quasi-Newton methods
+# solver{‘lbfgs’, ‘sgd’, ‘adam’}  sgd: stochastic gradient descent. adam: adaptive moment estimation. lbfgs: an optimizer in the family of quasi-Newton methods
 # activation{‘identity’, ‘logistic’, ‘tanh’, ‘relu’}
-mlp = MLPClassifier(hidden_layer_sizes=(10,10,10),random_state=1,activation = 'relu',solver='sgd', max_iter=2000)
+mlp = MLPClassifier(hidden_layer_sizes=(10,10,10),random_state=1,activation = 'relu',solver='adam', max_iter=2000)
 mlp.fit(X_train, y_train)
 
 #run the model off the test data
@@ -55,41 +64,55 @@ cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
 sns.heatmap(cnf_matrix, annot=True)
 plt.show()
 
-#fig, ((ax1)) = plt.subplots(nrows=1, ncols=1, figsize=(5,5)) #sharex=True, sharey=True,
-#plt.subplot(1,1, 1)
-#plt.savefig(os.path.join(dirName + '/' + Run_settings['CoreOfStudy'] + '_' + Formation_names + '_PCA' + '.png'),dpi = 300)
-
-#####run the model across the entire dataset to add a column of predicted chemofacies to the exported data sheet
-
-chemo_predict=mlp.predict(X_total) #scaled original dataset. 
-chemo_predict=chemo_predict.reshape(-1, 1)
-#chemo_predict=pd.DataFrame(data=chemo_predict,columns=["predicted"])
-chemo_prob=mlp.predict_proba(X_total)
-
-## Issues here. THe number of Probabilities should be dependant on on number of chemifacies defined for Formation
-
-Chemofacies_count=np.sort(NeuralModel_TrainingDataSet['Chemofacies_NN'].unique())
-
-Prediction_matrix_headings=['Chemo_pred_wireline']
-
-for i in range(len(Chemofacies_count)):
-    Prediction_matrix_headings.append('Prob'+str(Chemofacies_count[i]))
-
-
-data=pd.DataFrame(np.concatenate((chemo_predict,chemo_prob),axis=1),columns = Prediction_matrix_headings)
-
+# Output Neural network model
 NN_wirelinefile=os.path.join(Root_path + '/CoreData/CoreNeuralModel/' + 'NN_model_wireline_' + Formation_names)
-
 outfile = open(NN_wirelinefile,'wb')
 pickle.dump(mlp,outfile)
 outfile.close()
 
 
+
+##### XGBoost model######
+
+
+y=NeuralModel_TrainingDataSet['Chemofacies_XGB']
+X = NeuralModel_TrainingDataSet[Model_logs].values #converts X from a df to an array
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20,random_state=0)
+
+feature_names = Model_logs
     
+dtrain = xgb.DMatrix(X_train, label=y_train,feature_names=feature_names)
+dtest = xgb.DMatrix(X_test, label=y_test,feature_names=feature_names)
+
+param = {
+    'max_depth': 10,  # the maximum depth of each tree. too high and will overfit. Noticed with Iris dataset that if the number is less than the number of features it skips a feature
+    'eta': 0.3,  # the training step for each iteration
+    'silent': 1,  # logging mode - quiet
+    'objective': 'multi:softprob',  # error evaluation for multiclass training
+    'num_class': 9}  # the number of classes that exist in this datset
+num_round = 5  # the number of training iterations
+
+XGB_model = xgb.train(param, dtrain, num_round) #XGBoost model 
+
+preds = XGB_model.predict(dtest) # dtest is built off of X_test
+y_pred_XGB = np.asarray([np.argmax(line) for line in preds]) # an array of the predictions for each sample (largest value)
+
+#Feature importance
+
+plot_importance(XGB_model)
+plt.show()
 
 
+cnf_matrix = metrics.confusion_matrix(y_test, y_pred_XGB)
+sns.heatmap(cnf_matrix, annot=True)
+plt.show()
 
+# Export XGBoost model 
+XGB_wirelinefile=os.path.join(Root_path + '/CoreData/CoreNeuralModel/' + 'XGB_model_wireline_' + Formation_names)
 
+outfile = open(XGB_wirelinefile,'wb')
+pickle.dump(XGB_model,outfile)
+outfile.close()
 
 
 
