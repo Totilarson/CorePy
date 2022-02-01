4e77844de4c1606f1eb358f0a5dd0bf58b507feb
Totilarson <65969309+Totilarson@users.noreply.github.com>
2022-01-31 20:48:50 -0600

    changed initial core

--- CorePycodes/settings.py
+++ CorePycodes/settings.py
@@ -9,16 +9,16 @@ import pickle
 
 Run_settings = {
 # CoreOfStudy, Depth_model, Formation, Formation_2 define core name (.csv file), Formation (column in .csv file), and Depth_model for plotting (column in.csv file)    
-"CoreOfStudy" : 'LloydHurt',
-"Depth_model" : 'Wireline_Depth',        # 'XRF_adjusted_depth', 'Depth_calculated' and 'Wireline_Depth' are options in the data file. 
-"Formation" : ['Austin Chalk'], # Filter the Formation column by specific formations 'Austin Chalk', 'Eagle Ford', 'Wolfcamp', Bone Spring'
+"CoreOfStudy" : 'Public',
+"Depth_model" : 'Depth_calculated',        # 'XRF_adjusted_depth', 'Depth_calculated' and 'Wireline_Depth' are options in the data file. 
+"Formation" : ['Public Formation'], # Filter the Formation column by specific formations 'Austin Chalk', 'Eagle Ford', 'Wolfcamp', Bone Spring'
 "Formation_2" : [] , # Leave blank for now.  This function can be used to sample members within a formation.
 
 #RockClassification and Electrofacies are used for plotting. These are outputs in csv file from machine learning classifications
 # both XGBoost  (Chemofacies_XGB) and Neural Network (Chemofacies_NN) classifications are calculated. 
 #User can chose which to apply to figures adn subsequent calculations
 "RockClassification" : 'Chemofacies_NN',   # Chemofacies_PCA, Chemofacies_NN, Chemofacies_XGB, and Chemofacies_train are options
-"Electrofacies" : "Electrofacies_XGB",
+"Electrofacies" : 'Electrofacies_NN', # 'Electrofacies_XGB' or 'Electrofacies_NN'
 
 # 'elements and Elements_plotted needs to be reconsidered
 # 'elements' lists all the elements used in PCA and machine learning classifications. Can be changed depending on what is available or selected
@@ -37,7 +37,7 @@ Run_settings = {
 
 
 # noOfCols and ImageType are for original core photographs
-'noOfCols' :  5,            # select number of columns in each corebox photo
+'noOfCols' :  7,            # select number of columns in each corebox photo
 'ImageType' : 'vis',        # visible or UV images
 }
 
@@ -54,6 +54,10 @@ if Run_settings['Formation'] == ['Bone Spring']:
     Run_settings['ColorScheme'] =  [0   ,1      ,4    ,3      ,2      ,8      ,7    ,5       ,6     ,9 ]  #Bone Spring
 if Run_settings['Formation'] == ['Bone Spring_Lime']:
     Run_settings['ColorScheme'] = [0   ,1      ,4    ,3      ,2      ,7      ,6    ,999       ,5     ,8 ]  #Bone Spring_Lime
+if Run_settings['Formation'] == ['Bone Spring_Lime']:
+    Run_settings['ColorScheme'] = [0   ,1      ,4    ,3      ,2      ,7      ,6    ,999       ,5     ,8 ]  #Bone Spring_Lime
+if Run_settings['Formation'] == ['Public Formation']:
+    Run_settings['ColorScheme'] = [0   ,1      ,4    ,3      ,2      ,7      ,6    ,999       ,5     ,8 ]  #Public Formation
 
 # This is an additional function that is not applied yet. Can be expanded to included FOrmation_2, but not used yet
 # 
