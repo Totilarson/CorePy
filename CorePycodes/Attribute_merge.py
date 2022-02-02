9718011b72af69b627b9dc8007b662c0dc335dea
Toti Larson <toti.larson@beg.utexas.edu>
2022-02-02 08:30:40 -0600

    fixed directory

--- CorePycodes/Attribute_merge.py
+++ CorePycodes/Attribute_merge.py
@@ -1,54 +1,43 @@
-#merge tables closest value
-import pandas as pd
 import os
+import pandas as pd
 import json
 import corepytools as corepy
 
 
+# Attribute_merge.py is run after PCAexample and NN_Model_apply.py
+# File dependencies: 1) input file _XRF.csv data file in folder //CorePy/CoreOutput/CoreName/<Formation>
+# File dependencies: 2) input attribute files in //CorePy/CoreData/CoreAttributes/<core name>
+# Output: adds attribute csv files in //CorePy/CoreOutput/CoreName/<Formation>
+
+# Root_path, Run_settings, and Corebeta and the two .json core settings files with all input parameters
 Root_path = os.path.dirname(os.getcwd())
 Run_settings=json.load(open(os.path.join(Root_path + '/CorePycodes/' + 'Run_settings' + '.json')))
 Corebeta=json.load(open(os.path.join(Root_path + '/CoreData/CoreBeta/'   +  Run_settings['CoreOfStudy']  +'.json')))
 
-Formation_names = '-'.join(Run_settings["Formation"]+Run_settings["Formation_2"]) # Would like to have Formation_names defined in Corebeta
+# Formation_names is an expansion idea to select sub-Formations
+# Creates a str variable to select Formation-specific rows from csv input file. 
+# For now Formation_names is Run_settings["Formation"]
+Formation_names=corepy.Formation_names(Run_settings["Formation"],Run_settings["Formation_2"])
+# RootDir(corename, Formation_names) established the output folder structure
+dirName=corepy.RootDir(Corebeta["corename"], Formation_names) 
 
-XRF_data = corepy.OutputXRF(Run_settings['CoreOfStudy'],Formation_names) # This directs to the output file
+# Searches for all csv files in attribute folder. The attribute data have to be depth references (core, box, inch) to match
+Attribute_dir = os.path.join(Root_path + '/CoreData/CoreAttributes/'   +  Run_settings['CoreOfStudy'])
+all_files = os.listdir(Attribute_dir)    
+csv_files = list(filter(lambda f: f.endswith('.csv'), all_files))
 
-Attribute_dir = os.path.join(Root_path + '/CoreData/CoreAttributes/' + Run_settings['CoreOfStudy'] +'/') 
+# Get the path directory for the XRF file name
 
-Combined_file=XRF_data
-lst=[]
-for i in range(len(os.listdir(Attribute_dir))):
+XRF = corepy.OutputXRF(Run_settings["CoreOfStudy"],Formation_names)
+Merged_file=XRF
+
+# Loop over the attribute files and merge on Core-Box-Inch
+for i in range(len(csv_files)):
     
-    File_merge= os.path.join(Attribute_dir + os.listdir(Attribute_dir)[i]) #grabs the files in the Attribute folder _XRD, _TOC...
-    File_merge=pd.read_csv(File_merge)
-    Q=(list(set(list(File_merge))-set(list(XRF_data)))) # finds the column headings that are different and turns into a list
-    
-    File_merge = pd.merge(Combined_file, File_merge, how='left', on=['Core','Box', 'Inch'])
-    
-    Combined_file=File_merge # allows it to loop through and add to XRF_data dataframe
-    
-    lst.extend([Q]) # appends the list made of column headings
-
-AttributeHeadings=[]
-for i in range(len(lst)):    
-    #df=pd.DataFrame(lst)
-    AttributeHeadings= AttributeHeadings+lst[i]
+    Att_file = pd.read_csv(os.path.join(Attribute_dir + '/' + csv_files[i]))
+    # Merging adds duplicate file names so I use these two lines to remove duplicate names
+    Merged_file = pd.merge(Merged_file, Att_file, how='left', on=['Core', 'Box', 'Inch'],suffixes=('', '_drop'))  
+    Merged_file.drop([col for col in Merged_file.columns if 'drop' in col], axis=1, inplace=True)
 
 
-
-Corebeta['AttributeHeadings'] = AttributeHeadings
-with open(os.path.join(Root_path + '/CoreData/CoreBeta/'   + Run_settings['CoreOfStudy']  + '.json'), 'w') as f:    
-    json.dump(Corebeta, f)  
-
-dirName=corepy.RootDir(Run_settings['CoreOfStudy'], Formation_names) 
-
-Combined_file=File_merge.loc[:,~Combined_file.columns.str.contains('_y')]
-
-#Combined_file.columns = Combined_file.columns.str.rstrip('_x')  # strip suffix at the right end only.
-#Combined_file.columns = Combined_file.columns.str.strip('_x')
-
-
-Combined_file.to_csv (os.path.join(dirName + '/' +  Run_settings['CoreOfStudy'] + '_' + Formation_names + '_Attribute.csv'))
-
-    #df[Corebeta['WirelineLogs'][i]] =  new_data[:, [1]]
-    
+Merged_file.to_csv (os.path.join(dirName + '/' +  Run_settings["CoreOfStudy"] + '_' + Formation_names + '_Attribute.csv'))
